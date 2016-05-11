f85920cc5c3c3f296858fe79de0f1d7c4b4af6ff
AtwoodDeng <dengjdtsinghua@gmail.com>
2016-05-10 23:40:46 -0700

    add change view & auto see the selection window in the beginning

---
+++ Assets/Script/UI/Basic/CameraHolder.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraHolder : MonoBehaviour {
+
+	[SerializeField] GameObject selectionWindow;
+
+	void Awake()
+	{
+		RotateTowardSelection();
+	}
+
+	void RotateTowardSelection( )
+	{
+		if ( selectionWindow == null )
+			selectionWindow = GameObject.FindGameObjectWithTag( "SelectionWindow" );
+		
+		transform.LookAt( selectionWindow.transform.position );
+
+	}
+
+	void OnFingerMove( FingerMotionEvent e ) {
+		
+		Vector3 from = Camera.main.ScreenPointToRay( e.Position - e.Finger.DeltaPosition ).direction;
+		Vector3 to = Camera.main.ScreenPointToRay( e.Position ).direction;
+
+		from.y = to.y = 0;
+		transform.rotation *= Quaternion.FromToRotation( from , to );
+	}
+
+}
