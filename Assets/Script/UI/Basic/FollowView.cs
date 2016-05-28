78e334c22d61b0b25ced5d5203294493e64c2b36
AtwoodDeng <dengjdtsinghua@gmail.com>
2016-05-28 22:59:52 +0800

    play tool bar follow the camera

---
+++ Assets/Script/UI/Basic/FollowView.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowView : MonoBehaviour {
+
+	float initDistance;
+
+	void Awake()
+	{
+		Vector3 dis = transform.position - Camera.main.transform.position;
+		dis.y = 0;
+		initDistance = dis.magnitude;
+	}
+
+	void LateUpdate()
+	{
+		Vector3 cameraForward = Camera.main.transform.forward;
+		cameraForward.y = 0;
+		Vector3 toPos = cameraForward.normalized * initDistance;
+		toPos.y = transform.position.y;
+		transform.position = toPos;
+
+		transform.rotation = Quaternion.LookRotation( - cameraForward );
+
+	}
+
+
+}
