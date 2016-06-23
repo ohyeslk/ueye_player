4571458cf1fc5f79500ab67c6ad103520bfc1af1
weijia yu <weijia yu>
2016-06-23 14:02:41 -0700

    add chat client

---
+++ Assets/comment.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class comment : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void moveUp()
+    {
+        this.transform.DOLocalMoveY(70, 0.25f).SetRelative(); ;
+    }
+}
