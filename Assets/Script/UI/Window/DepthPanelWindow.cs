144a936e17aa29bc772545eff57408af7f358c38
Chong Li <manymany.chong@gmail.com>
2016-07-06 13:50:34 -0700

    demo commit

--- Assets/Script/UI/Window/DepthPanelWindow.cs
+++ Assets/Script/UI/Window/DepthPanelWindow.cs
@@ -7,7 +7,8 @@ public class DepthPanelWindow : MonoBehaviour {
 	[SerializeField] MediaPlayerCtrl videoPlayer;
 
 //	const string m_url = "http://d2fn2cfzrmdwta.cloudfront.net/liveout/s/playlist.m3u8";
-	public static string m_url = "http://balalalive-38f2.kxcdn.com/liveout/s/playlist.m3u8";
+	public static string m_url = "http://balalalivecf-38f2.kxcdn.com/liveout/s/playlist.m3u8";
+//	public static string m_url = "http://sc-hls-live.speedycloud.cn/live/fac20fa7b255c91125401009c4409301.m3u8";
 
 	void Awake()
 	{
