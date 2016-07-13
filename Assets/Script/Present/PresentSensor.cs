78ca41640bfe8c337d79da82cb7bf8bc92bddafe
AtwoodDeng <dengjdtsinghua@gmail.com>
2016-07-13 16:01:18 -0700

    for save

--- Assets/Script/Present/PresentSensor.cs
+++ Assets/Script/Present/PresentSensor.cs
@@ -3,19 +3,22 @@ using System.Collections;
 
 public class PresentSensor : MonoBehaviour {
 
-	public void Init ( PresentUnit _pre )
+	PresentUnit parent;
+
+	public void Init ( PresentUnit _p )
 	{
-		
+		parent = _p;	
 	}
+
 	void OnFingerDown( FingerDownEvent e ) {
-		
+		parent.OnFingerDown( e );	
 	}
 
 	void OnFingerUp( FingerUpEvent e ) {
-		
+		parent.OnFingerUp( e );
 	}
 
 	void OnFingerMove( FingerMotionEvent e ) {
-		
+		parent.OnFingerMove( e );
 	}
 }
--- Assets/Script/Present/PresentUnit.cs
+++ Assets/Script/Present/PresentUnit.cs
@@ -3,10 +3,43 @@ using System.Collections;
 
 public class PresentUnit : MonoBehaviour {
 
+	[SerializeField]SphereCollider col;
+
 	void Start()
 	{
-		Collider col = GetComponentInChildren<Collider>();
-		PresentSensor sensor = col.gameObject.AddComponent<PresentSensor>();
-		sensor.Init( this );
+		if ( col == null )
+			col = GetComponentInChildren<SphereCollider>();
+
+		if ( col != null )
+		{
+			PresentSensor sensor = col.gameObject.AddComponent<PresentSensor>();
+			sensor.Init( this );
+		}
+	}
+
+	public void OnFingerDown( FingerDownEvent e ) {
+		
+	}
+
+	public void OnFingerUp( FingerUpEvent e ) {
+
+	}
+
+	public void OnFingerMove( FingerMotionEvent e ) {
+		if ( e.Phase == FingerMotionPhase.Started )
+		{
+			col.radius *= 3f;
+		}
+		else if ( e.Phase == FingerMotionPhase.Ended )
+		{
+			Vector3 screenPos = Camera.main.WorldToScreenPoint( transform.position );
+			Vector3 newScreenPos = screenPos + new Vector3( e.Position.x , e.Position.y , 0 );
+
+			float distance = ( transform.position - Camera.main.transform.position ).magnitude;
+		}
+		else if ( e.Phase == FingerMotionPhase.Updated )
+		{
+			col.radius /= 3f;
+		}
 	}
 }
